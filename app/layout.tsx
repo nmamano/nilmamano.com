d022bfa42edbc448471b5f66fae08a0181152da5
nmamano <nil.mamano@gmail.com>
2025-03-20 03:46:16 -0700

    Add og:image and Twitter card <meta> tags
    Inspired by https://x.com/levelsio/status/1901963649930928177

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,29 @@ export const metadata: Metadata = {
     ],
     apple: [{ url: "/favicon/apple-touch-icon.png" }],
   },
+  openGraph: {
+    title: "nilmamano.com",
+    description: "Computer scientist, software engineer, author.",
+    url: "https://nilmamano.com",
+    siteName: "Nil Mamano",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Nil Mamano - Computer scientist, software engineer, author.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "nilmamano.com",
+    description: "Computer scientist, software engineer, author.",
+    creator: "@Nil053",
+    images: ["/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
